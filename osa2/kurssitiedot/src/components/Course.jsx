51a366a56723a71bf3e6e37686fbe40140df7ef1
SamiP7 <sami.parkkinenn@gmail.com>
2024-07-10 15:55:53 +0300

    2.1

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,10 @@
+import Content from './Content'
+import Header from './Header'
+
+const Course = ({ course }) => 
+    <>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    </>
+
+export default Course
